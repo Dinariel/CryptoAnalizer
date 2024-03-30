084604894c675baaf8c5c43093ba1ea94d589d0c
Dinar <dinar.khidiyatov@gmail.com>
2024-03-30 19:13:03 +0500

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Khidiyatov project
---
+++ src/main/java/ru/javarush/khidiyatov/cryptoanalizer/Runner.java
@@ -0,0 +1,7 @@
+package ru.javarush.khidiyatov.cryptoanalizer;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("hello maven");
+    }
+}
